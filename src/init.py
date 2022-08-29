c2fddc6ce1d146c8f99f2af2033f0a4253a0bce8
nanometer5088 <invalid-email@email.com>
2022-08-29 19:26:15 -0300

    * setup.py can now install dependencies on its own * init.py is changed to better reflect the setup.py changes No core functionality was changed in this commit

--- src/init.py
+++ src/init.py
@@ -2,7 +2,7 @@ def inicio():
     import os
     os.system("cls || clear")
     lista = []
-    input("Avaliação Continuada 1 – Análise exploratória dos dados.\n\nCertifique-se que tenha as bibliotecas necessárias instaladas\nAs bibliotecas podem ser instaladas usando 'pip install -r requirements.txt'\n\nPressione ENTER para prosseguir")
+    input("Avaliação Continuada 1 – Análise exploratória dos dados.\n\nCertifique-se que tenha as bibliotecas necessárias instaladas\nAs bibliotecas podem ser instaladas rodando o arquivo 'setup.py'\n\nPressione ENTER para prosseguir")
     os.system("cls || clear")
     elementos = int(input("Insira a quantidade de elementos da lista: "))
     os.system("cls || clear")
