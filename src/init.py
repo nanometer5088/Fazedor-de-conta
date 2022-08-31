c5864811ca72dc3a9897c967651affb7a95929cf
nanometer5088 <invalid-email@email.com>
2022-08-31 09:47:29 -0300

    Welcome Screen and warnings now only show once!

--- src/init.py
+++ src/init.py
@@ -2,7 +2,15 @@ def inicio():
     import os
     os.system("cls || clear")
     lista = []
-    input("Avaliação Continuada 1 – Análise exploratória dos dados.\n\nCertifique-se que tenha as bibliotecas necessárias instaladas\nAs bibliotecas podem ser instaladas rodando o arquivo 'setup.py'\n\nPressione ENTER para prosseguir")
+    try:
+        config = open('config.json', 'r', encoding='utf-8')
+        config.close()
+    except FileNotFoundError:
+        config = open('config.json', 'w')
+        config.write('Tutorial_Done=1')
+        config.close()
+        input("Avaliação Continuada 1 – Análise exploratória dos dados.\n\nCertifique-se que tenha as bibliotecas necessárias instaladas\nAs bibliotecas podem ser instaladas rodando o arquivo 'setup.py'\nEssa mensagem só será mostrada uma vez\n\nPressione ENTER para prosseguir")
+    
     os.system("cls || clear")
     elementos = int(input("Insira a quantidade de elementos da lista: "))
     os.system("cls || clear")
